c4a86814d3263e2dadbfe4ad2c83aeefd56778ce
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-07-17 01:11:46 +0500

    feat: add the reactive form

--- web/src/app/components/conversions/conversions.ts
+++ web/src/app/components/conversions/conversions.ts
@@ -1,28 +1,32 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { IConversion } from '../../interfaces/conversion';
 import { MatTableModule } from '@angular/material/table';
 import { CurrencyService } from '../../services/currency-service';
-import { Subject, takeUntil } from 'rxjs';
+import { IConversion } from '../../interfaces/conversion';
+import { Subject, takeUntil, tap, filter } from 'rxjs';
 import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-conversions',
   imports: [MatCardModule, MatTableModule, DatePipe],
-  providers: [CurrencyService],
   templateUrl: './conversions.html',
   styleUrl: './conversions.scss'
 })
 export class Conversions implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
-  dataSource: IConversion[] = []
-
+  dataSource: IConversion[] = [];
   columns = ['id', 'fromCurrency', 'toCurrency', 'amount', 'result', 'date'];
 
-  constructor(private currencyService: CurrencyService) { }
+  constructor(private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
-    this.getConversions();
+    this.getConversions(); // Fetch initial conversions
+    this.currencyService.conversionAdded
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(res => console.log('Conversion added:', res)),
+      )
+      .subscribe(() => this.getConversions());
   }
 
   ngOnDestroy(): void {
@@ -34,6 +38,7 @@ export class Conversions implements OnInit, OnDestroy {
     this.currencyService.getConversions()
       .pipe(
         takeUntil(this.destroy$),
+        tap(data => console.log('Conversions fetched:', data)) // Debug
       )
       .subscribe(data => this.dataSource = data);
   }
